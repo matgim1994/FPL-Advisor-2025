c3c8349b90586522e2fb8871963b983e33750e75
matgim1994 <mateuszgimzinski@gmail.com>
2025-11-15 12:13:44 +0100

    create dotenv file function done

--- fpl.py
+++ fpl.py
@@ -1,5 +1,4 @@
 import argparse
-
 from src.db_handlers.db_handler import DBHandler
 from src.db_handlers.dbt_handler import DBTHandler
 from src.pg_config import get_pg_config
@@ -26,6 +25,25 @@ def update_raw(dbhandler: DBHandler, dbthandler: DBTHandler):
     dbthandler.run_dbt_tests()
 
 
+def create_dotenv():
+    fpl_pg_user = input('Your postgres master user name: ')
+    fpl_pg_password = input('Your postgres master user password: ')
+    fpl_pg_db = input('Your database name: ')
+    fpl_pg_host = input('Your db host (write localhost if working locally): ')
+    fpl_pg_port = input('Your database port: ')
+
+    env_vars = {
+        "FPL_PG_USER": fpl_pg_user,
+        "FPL_PG_PASS": fpl_pg_password,
+        "FPL_PG_DB": fpl_pg_db,
+        "FPL_PG_HOST": fpl_pg_host,
+        "FPL_PG_PORT": fpl_pg_port,
+    }
+
+    with open("./.env", "w") as file:
+        file.write("\n".join(f"{k}={v.strip()}" for k, v in env_vars.items()))
+
+
 def main():
     pgconfig = get_pg_config()
     dbhandler = DBHandler(pgconfig=pgconfig)
@@ -33,10 +51,10 @@ def main():
 
     parser = argparse.ArgumentParser(
         description=(
-            "This is a CLI for backend of the FPL Advisor app.\n"
-            "You can use it to setup your db, update it, prune old data,\n"
-            "run Django REST API or even schedule db updates for the future.\n"
-            "Checkout possible actions in the description below."
+            """This is a CLI for backend of the FPL Advisor app.
+            You can use it to setup your db, update it, prune old data,
+            run Django REST API or even schedule db updates for the future.
+            Checkout possible actions in the description below."""
         ),
         formatter_class=argparse.RawTextHelpFormatter
     )
@@ -57,7 +75,7 @@ def main():
         '-ur',
         '--update-raw',
         action='store_true',
-        help="""Use this flag to fetch fresh data to raw tables."""
+        help="Use this flag to fetch fresh data to raw tables."
     )
 
     parser.add_argument(
@@ -65,6 +83,13 @@ def main():
         '--run-dbt',
         action="store_true",
         help="Use this flag to run dbt transformations and tests."
+    )
+
+    parser.add_argument(
+        '-cd',
+        '--create-dotenv',
+        action='store_true',
+        help="Use this flag to create dotenv file with postgres credentials."
     )
     args = parser.parse_args()
 
@@ -74,6 +99,8 @@ def main():
         dbhandler.update_raw()
     elif args.run_dbt:
         run_dbt(dbthandler=dbthandler)
+    elif args.create_dotenv:
+        create_dotenv()
     else:
         print("Script used without specified command. Run -h to checkout possibilities.")
 
